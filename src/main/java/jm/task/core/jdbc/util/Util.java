0507746766daf99a15790122007c84c1b93b4dea
DmitriyDruzhkov <sedyar@gmail.com>
2024-12-18 12:03:21 +0300

    Update Util.java

--- src/main/java/jm/task/core/jdbc/util/Util.java
+++ src/main/java/jm/task/core/jdbc/util/Util.java
@@ -5,10 +5,6 @@ import java.sql.*;
 public class Util {
     private static Connection connection;
 
-    public Util(Connection connection) {
-        this.connection = connection;
-    }
-
     public static Connection getConnection() {
         try {
             Connection connection = DriverManager.getConnection(
